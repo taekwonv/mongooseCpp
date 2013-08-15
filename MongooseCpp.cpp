22e0bd1d7d656e8dfe6952f24d24ff59f8522342
thkim <thkim@nexon.net>
2013-08-14 21:49:55 -0700

    The part of request handler was implemented.

--- MongooseCpp.cpp
+++ MongooseCpp.cpp
@@ -11,58 +11,17 @@
 
 
 
-MgServer::MgServer(void)
-{
-}
+MgServer::MgServer(void) : m_listener(NULL) {}
 
-
-MgServer::~MgServer(void)
-{
-}
-
-
-MgServerImpl::MgServerImpl()
-	: m_ctx(NULL)
-{
-	this->begin_request = &MgServerImpl::s_begin_request;
-}
-
-int MgServerImpl::member_begin_request(struct mg_connection *)
-{
-	return 0;
-}
-
-int MgServerImpl::s_begin_request(struct mg_connection *con)
-{
-	mg_request_info *info = mg_get_request_info(con);
-	MgServerImpl *server = reinterpret_cast<MgServerImpl *>(info->user_data);
-	return server->member_begin_request(con);
-}
-
-void MgServerImpl::s_end_request(const struct mg_connection *con, int reply_status_code)
-{	
-}
-
-int MgServerImpl::s_log_message(const struct mg_connection *, const char *message){ return 0; }
-int MgServerImpl::s_init_ssl(void *ssl_context, void *user_data){ return 0; }
-int MgServerImpl::s_websocket_connect(const struct mg_connection *){ return 0; }
-void MgServerImpl::s_websocket_ready(struct mg_connection *){}
-int MgServerImpl::s_websocket_data(struct mg_connection *, int bits,
-				char *data, size_t data_len){ return 0; }
-const char * MgServerImpl::s_open_file(const struct mg_connection *,
-					const char *path, size_t *data_len){ return NULL; }
-void MgServerImpl::s_init_lua(struct mg_connection *, void *lua_context){}
-void MgServerImpl::s_upload(struct mg_connection *, const char *file_name){}
-int MgServerImpl::s_http_error(struct mg_connection *, int status){ return 0; }
-
+MgServer::~MgServer(void) {}
 
 MgServer *MongooseCpp::CreateServer(MongooseCpp::Config &cfg)
 {
 	MgServerImpl *server = new MgServerImpl;
 	
 	const char *options[] = {
-		"document_root", "/var/www",
-		"listening_ports", "80,443",
+		"document_root", "html",
+		"listening_ports", "8080",
 		NULL
 	};
 
@@ -76,4 +35,216 @@ MgServer *MongooseCpp::CreateServer(MongooseCpp::Config &cfg)
 	server->context(ctx);
 
 	return server;
+}
+
+
+/**
+ *  _____     _____                     _____           _ 
+ * |     |___|   __|___ ___ _ _ ___ ___|     |_____ ___| |
+ * | | | | . |__   | -_|  _| | | -_|  _|-   -|     | . | |
+ * |_|_|_|_  |_____|___|_|  \_/|___|_| |_____|_|_|_|  _|_|
+ *       |___|                                     |_|    
+ */                                                       
+
+
+static int static_begin_request(struct mg_connection *con) 
+{
+	mg_request_info *info = mg_get_request_info(con);
+	MgServerImpl *server = reinterpret_cast<MgServerImpl *>(info->user_data);
+	if (NULL == server) return 0; 
+	return server->member_begin_request(con);
+}
+
+static void static_end_request(const struct mg_connection *con, int reply_status_code) 
+{
+	mg_request_info *info = mg_get_request_info(const_cast<struct mg_connection *>(con));
+	MgServerImpl *server = reinterpret_cast<MgServerImpl *>(info->user_data);
+	if (NULL == server) return; 
+	return server->member_end_request(con, reply_status_code);
+}
+
+static int static_log_message(const struct mg_connection *con, const char *message) 
+{
+	mg_request_info *info = mg_get_request_info(const_cast<struct mg_connection *>(con));
+	MgServerImpl *server = reinterpret_cast<MgServerImpl *>(info->user_data);	
+	if (NULL == server) return 0; 
+	return server->member_log_message(con, message);
+}	
+
+static int static_websocket_connect(const struct mg_connection *con) 
+{ 
+	mg_request_info *info = mg_get_request_info(const_cast<struct mg_connection *>(con));
+	MgServerImpl *server = reinterpret_cast<MgServerImpl *>(info->user_data);
+	if (NULL == server) return 0; 
+	return server->member_websocket_connect(con);
+}
+
+static void static_websocket_ready(struct mg_connection *con) 
+{
+	mg_request_info *info = mg_get_request_info(con);
+	MgServerImpl *server = reinterpret_cast<MgServerImpl *>(info->user_data);
+	if (NULL == server) return; 
+	return server->member_websocket_ready(con);
+}
+
+static int static_websocket_data(struct mg_connection *con, int bits, char *data, size_t data_len) 
+{
+	mg_request_info *info = mg_get_request_info(con);
+	MgServerImpl *server = reinterpret_cast<MgServerImpl *>(info->user_data);
+	if (NULL == server) return 0; 
+	return server->member_websocket_data(con, bits, data, data_len);
+}
+
+static const char * static_open_file(const struct mg_connection *con, const char *path, size_t *data_len) 
+{
+	mg_request_info *info = mg_get_request_info(const_cast<struct mg_connection *>(con));
+	MgServerImpl *server = reinterpret_cast<MgServerImpl *>(info->user_data);
+	if (NULL == server) return NULL; 
+	return server->member_open_file(con, path, data_len);
+}
+
+static void static_init_lua(struct mg_connection *con, void *lua_context) 
+{
+	mg_request_info *info = mg_get_request_info(con);
+	MgServerImpl *server = reinterpret_cast<MgServerImpl *>(info->user_data);
+	if (NULL == server) return;
+	return server->member_init_lua(con, lua_context);
+}
+
+static void static_upload(struct mg_connection *con, const char *file_name) 
+{
+	mg_request_info *info = mg_get_request_info(con);
+	MgServerImpl *server = reinterpret_cast<MgServerImpl *>(info->user_data);
+	if (NULL == server) return; 
+	return server->member_upload(con, file_name);
+}
+
+static int static_http_error(struct mg_connection *con, int status) 
+{
+	mg_request_info *info = mg_get_request_info(con);
+	MgServerImpl *server = reinterpret_cast<MgServerImpl *>(info->user_data);
+	if (NULL == server) return 0; 
+	return server->member_http_error(con, status);
+}
+
+MgServerImpl::MgServerImpl()
+	: m_ctx(NULL)
+{
+	// map callbacks to member funcs		
+	this->begin_request = static_begin_request;
+	this->end_request = static_end_request;
+	this->log_message = static_log_message;
+	this->init_ssl = s_init_ssl;
+	this->websocket_connect = static_websocket_connect;
+	this->websocket_ready = static_websocket_ready;
+	this->websocket_data = static_websocket_data;
+	this->open_file = static_open_file;
+	this->init_lua = static_init_lua;
+	this->upload = static_upload;
+	this->http_error = static_http_error;
+}
+
+int MgServerImpl::member_begin_request(struct mg_connection *con)
+{
+	if (m_listener)
+	{
+		mg_request_info *info = mg_get_request_info(con);
+		MgResponse *res = new MgResponse(con);
+		MgRequest *req = new MgRequest;
+		m_listener(req, res);
+	}
+	return 0;
+}
+
+void MgServerImpl::member_end_request(const struct mg_connection *, int reply_status_code)
+{
+}
+
+int MgServerImpl::member_log_message(const struct mg_connection *, const char *message)	
+{
+	return 0;
+}
+
+int MgServerImpl::member_websocket_connect(const struct mg_connection *)
+{
+	return 0;
+}
+
+void MgServerImpl::member_websocket_ready(struct mg_connection *)
+{
+}
+
+int MgServerImpl::member_websocket_data(struct mg_connection *, int bits, char *data, size_t data_len)
+{
+	return 0;
+}
+
+const char *MgServerImpl::member_open_file(const struct mg_connection *, const char *path, size_t *data_len)
+{
+	return NULL;
+}
+
+void MgServerImpl::member_init_lua(struct mg_connection *, void *lua_context)
+{
+}
+
+void MgServerImpl::member_upload(struct mg_connection *, const char *file_name)
+{
+}
+
+int MgServerImpl::member_http_error(struct mg_connection *, int status)
+{
+	return 0;
+}
+
+int MgServerImpl::s_init_ssl(void *ssl_context, void *user_data)
+{
+	return 0;
+}
+
+
+/**
+ *  _____     _____                             
+ * |     |___| __  |___ ___ ___ ___ ___ ___ ___ 
+ * | | | | . |    -| -_|_ -| . | . |   |_ -| -_|
+ * |_|_|_|_  |__|__|___|___|  _|___|_|_|___|___|
+ *       |___|             |_|                  
+ */
+MgResponse::MgResponse(struct mg_connection *con) 
+	: m_connection(con)
+{
+}
+
+void MgResponse::setHeader(int statusCode, const char *httpVersion, const char *content_type, unsigned long content_length, const char *transfer_encoding)
+{
+	m_statusCode	= statusCode;	
+	m_httpVersion	= httpVersion;
+	m_contentType	= content_type;
+	m_contentLength	= content_length;
+	m_transferEncoding = transfer_encoding;
+}
+
+void MgResponse::writeHeader(int statusCode, const char *httpVersion, const char *content_type, unsigned long content_length, const char *transfer_encoding)
+{
+	int ret = mg_printf(m_connection, 
+		"%s %d %s\r\n"
+		"Cache: %s\r\n"
+		"Content-Type: %s\r\n"
+		,		
+		httpVersion, statusCode, 200 == statusCode ? "OK" : "Error",
+		m_cache ? "" : "no-cache",
+		content_type);
+	if (content_length > 0) int ret = mg_printf(m_connection, "Content-Length: %d\r\n", content_length);
+	if (transfer_encoding) int ret = mg_printf(m_connection, "Transfer-Encoding: %s\r\n", transfer_encoding);		
+	mg_printf(m_connection, "\r\n");
+}
+
+void MgResponse::write(const std::string &content)
+{
+	int ret = mg_write(m_connection, content.c_str(), content.length());
+}
+
+void MgResponse::end()
+{
+	mg_printf(m_connection, "");
 }
--- MongooseCpp.h
+++ MongooseCpp.h
@@ -10,7 +10,16 @@
 #define MONGOOSECPP_H
 
 #include "../mongoose/mongoose.h"
+#include <functional>
+#include <memory>
 
+
+/**
+ *	MgRequest
+ *
+ *	The object of this class is created internally and returned from MgServer::IListener::OnRequest(). It represents an in-progress request from the client side.
+ *
+ */
 
 class MgRequest
 {
@@ -21,25 +30,64 @@ protected:
 };
 
 
+/**
+ *	MgResponse
+ *
+ *	The object of this class is created internally and returned from MgServer::IListener::OnRequest(). All you want to respond to the client may be implemented by using this object.
+ *
+ */
 class MgResponse
 {
-public:
+public:	
+	void setCache(bool b) { m_cache = b; }
+	void setTimeout(unsigned short sec) { m_timeoutSec = sec; }
+	void setStatusCode(int statusCode) { m_statusCode = statusCode; }
+	void setHeader(int statusCode, const char *httpVersion, const char *content_type, unsigned long content_length, const char *transfer_encoding);
+	void writeHeader(int statusCode, const char *httpVersion, const char *content_type, unsigned long content_length, const char *transfer_encoding);
+	void write(const std::string &content);
+	void end();
+
+	MgResponse(struct mg_connection *);
 
 protected:
+	MgResponse() {}
 	mg_connection *m_connection;
+	bool m_cache;
+	int	m_statusCode;
+	unsigned short m_timeoutSec;
+	std::string m_httpVersion;
+	std::string m_contentType;
+	unsigned long m_contentLength;
+	std::string m_transferEncoding;
 };
 
 
+/**
+ *	MgServer
+ *
+ *	The object of this class is created internally and returned from MongooseCpp::CreateServer(). It represent a HTTP(S) server instance.
+ *
+ */
 class MgServer
 {
 public:
 	MgServer(void);
 	~MgServer(void);
 
-	void Stop();
+	void stop();
+	void listen(std::function<void(MgRequest *req, MgResponse *res)> f) { m_listener = f; }
+
+protected:
+	std::function<void(MgRequest *req, MgResponse *res)> m_listener;
 };
 
 
+/**
+ *	MgServerImpl
+ *
+ *	The implementation of MgServer class (Bridge Design Pattern).
+ *
+ */
 class MgServerImpl : public MgServer, 
 	public mg_callbacks
 {
@@ -50,25 +98,21 @@ public:
 	void context(mg_context *ctx) { m_ctx = ctx; }
 	const mg_context *context() const { return m_ctx; }
 
-protected:
+	// callbacks
 	int member_begin_request(struct mg_connection *);
-
-	mg_context *m_ctx;
-
-private:
-	static int s_begin_request(struct mg_connection *);
-	static void s_end_request(const struct mg_connection *, int reply_status_code);
-	static int s_log_message(const struct mg_connection *, const char *message);
+	void member_end_request(const struct mg_connection *, int reply_status_code);
+	int member_log_message(const struct mg_connection *, const char *message);	
+	int member_websocket_connect(const struct mg_connection *);
+	void member_websocket_ready(struct mg_connection *);
+	int member_websocket_data(struct mg_connection *, int bits, char *data, size_t data_len);
+	const char * member_open_file(const struct mg_connection *, const char *path, size_t *data_len);
+	void member_init_lua(struct mg_connection *, void *lua_context);
+	void member_upload(struct mg_connection *, const char *file_name);
+	int member_http_error(struct mg_connection *, int status);
 	static int s_init_ssl(void *ssl_context, void *user_data);
-	static int s_websocket_connect(const struct mg_connection *);
-	static void s_websocket_ready(struct mg_connection *);
-	static int s_websocket_data(struct mg_connection *, int bits,
-							char *data, size_t data_len);
-	static const char * s_open_file(const struct mg_connection *,
-								const char *path, size_t *data_len);
-	static void s_init_lua(struct mg_connection *, void *lua_context);
-	static void s_upload(struct mg_connection *, const char *file_name);
-	static int s_http_error(struct mg_connection *, int status);
+
+protected:
+	mg_context *m_ctx;
 };
 
 
